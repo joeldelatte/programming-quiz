0ee80680c2438744b3b485e69aa1fec0e5ce291e
Joel Delatte <joeldelatte@gmail.com>
2019-11-03 06:43:31 -0600

    adds further but incomplete functionality and no local storage

--- questions.js
+++ questions.js
@@ -7,12 +7,12 @@ var questions = [
     {
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      answer: "parentheses" 
     },
     {
       title: "The external javascript file must contain the <script> tag.",
       choices: ["true", "false", "truthy", "falsy"],
-      answer: "false"
+      answer: "false" 
     },
     {
       title: "How do yo write an if statement in Javascript?",
@@ -25,3 +25,79 @@ var questions = [
       answer: "for (i = 0; i <= 5; i++)"
     },
   ];
+
+var question = document.querySelector("#question"); //question.title
+var answerA = document.querySelector("#answerA"); // question.choices[0]
+var answerB = document.querySelector("#answerB"); //quesytion.choices[1]
+var answerC = document.querySelector("#answerC");
+var answerD = document.querySelector("#answerD");
+var inCorrect = document.querySelector("#correct");
+
+var timer = document.getElementById("time-count");
+var count = 90;
+
+var next = document.querySelector("#next");
+var nextIndex = 1;
+
+var score = 0
+
+
+function playtime() {
+    var countDown = setInterval(function() {
+        count--;
+        timer.textContent = "Time: " + count;
+
+        if (count <= 0) {
+        clearInterval(countDown);
+        window.location.href="save_score.html";
+        }
+
+    }, 1000);
+
+};
+
+onpageshow = function () {
+  question.textContent = questions[0].title;
+  answerA.textContent = questions[0].choices[0];
+  answerB.textContent = questions[0].choices[1];
+  answerC.textContent = questions[0].choices[2];
+  answerD.textContent = questions[0].choices[3];
+
+}
+
+// doesnt work, needs to apply to each question: 
+
+document.onclick=function(){
+  var clicker = window.event.srcElement.id;
+  if(clicker == "answerC"){
+    inCorrect.textContent = "Correct";
+    score++
+    console.log(score)
+    next.onclick = function() {
+      nextIndex = (nextIndex+1)%(questions.length);
+      question.textContent = questions[1].title;
+      answerA.textContent = questions[1].choices[0];
+      answerB.textContent = questions[1].choices[1];
+      answerC.textContent = questions[1].choices[2];
+      answerD.textContent = questions[1].choices[3];
+      
+      inCorrect.textContent = "";    
+    } 
+  }
+  else if ( clicker == "answerA" || clicker == "answerB" || clicker == "answerD" ){
+    inCorrect.textContent = "Incorrect";
+    if (count >= 15) {count = count - 15};
+    timer.textContent = "Time: " + count;
+  }
+  else {
+    inCorrect.textContent = "";
+  }
+};
+
+
+
+
+
+
+playtime()
+
--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var timer = document.querySelector("#time-count");
-var start = document.querySelector("#start-button");
+// var buttons = [answerA, answerB, answerC, answerD]
+// var answer;
+// buttons.forEach.onclick = function() {
+//   for (i = 0; i <= buttons.length; i++) {
+//     answer = buttons[i]
+//     if(answer. = true) {
+//       inCorrect.textContent = "Correct";
+//     }
+//     else {
+//       inCorrect.textContent = "Incorrect";
+//       count - 15;
+//     }
+//   }
+// }
+
+
